693c43e7f9c5fb936f50243d3422e525f255d4e7
yoshinaga <yoshinaga@keylink.co.jp>
2020-04-18 21:08:08 +0900

    スペース

--- face_recognition/__main__.py
+++ face_recognition/__main__.py
@@ -12,19 +12,23 @@ video_capture = cv2.VideoCapture(0)
 
 while(True):
   ret, capture_frame = video_capture.read()
-  capture_frame = cv2.resize(capture_frame, (0, 0), fx=0.5, fy=0.5)
-  face_image_gray = cv2.cvtColor(capture_frame, cv2.COLOR_BGR2GRAY)
+  capture_frame      = cv2.resize(capture_frame, (0, 0), fx=0.5, fy=0.5)
+  face_image_gray    = cv2.cvtColor(capture_frame, cv2.COLOR_BGR2GRAY)
   #minNeighbors,minSizeは物体候補判定
   face_rect = cascade.detectMultiScale(
-      face_image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+                face_image_gray,
+                scaleFactor=1.1,
+                minNeighbors=2,
+                minSize=(30, 30)
+              )
 
-  color = (255, 255, 255)
+  outline_color = (255, 255, 255)
 
   #unknownが顔として認識された場合
   if len(face_rect) > 0:
     for rect in face_rect:
       cv2.rectangle(capture_frame, tuple(rect[0:2]), tuple(
-          rect[0:2]+rect[2:4]), color, thickness=2)
+          rect[0:2]+rect[2:4]), outline_color, thickness=2)
     cv2.imwrite(output_path, capture_frame)
 
   cv2.imshow('frame', capture_frame)
