aab1e8cf7b7343780580d537f318f2aeae115082
IbukiYoshinaga <yoshinaga.git@gmail.com>
2020-04-21 05:28:29 +0900

    出戻り

--- backend/remote_camera/__main__.py
+++ backend/remote_camera/__main__.py
@@ -1,17 +1,4 @@
 # -*- coding: utf-8 -*-
-import webbrowser
-from flask import Flask, Response
+from modules.camera_module import start_camera
 
-from modules.camera_module import get_stream_data
-
-app = Flask(__name__)
-
-stream_data = get_stream_data()
-
-@app.route('/')
-def video_stream():
-    return Response(stream_data, mimetype='multipart/x-mixed-replace; boundary=boundary')
-
-webbrowser.get().open("http://localhost:8080/")
-
-app.run(host='0.0.0.0', port = 8080, threaded = False)
+start_camera()
--- backend/remote_camera/modules/camera_module.py
+++ backend/remote_camera/modules/camera_module.py
@@ -1,33 +1,15 @@
 # -*- coding: utf-8 -*-
 import cv2
 from uuid import getnode as get_mac
+from datetime import datetime
 
 # ビデオ設定
 video_capture = cv2.VideoCapture(0)
-video_width   = int(video_capture.get(3))  #カメラの横幅
-video_height  = int(video_capture.get(4))  #カメラの縦幅
-fourcc        = cv2.VideoWriter_fourcc(*'XVID')
-video         = cv2.VideoWriter(
-                  'remote_camera/images/stream_video.avi',
-                  fourcc,
-                  30,
-                  (video_width, video_height)
-                )  #ビデオ設定
 
-def get_stream_data():
+def start_camera():
   while True:
     ret, capture_frame = video_capture.read()
-    camera_recording(False, capture_frame)
-    ret, jpg = cv2.imencode("stream_picture.jpg", capture_frame)
-    yield b'--boundary\r\nContent-Type: image/jpeg\r\n\r\n' + jpg.tostring() + b'\r\n\r\n'
-
-  video_capture.release()
-
-
-# 動画保存処理
-def camera_recording(release_flag=False, capture_frame=None):
-  if(capture_frame is not None):
-    video.write(capture_frame)
-
-  if(release_flag == True):
-    video.release()
+    date = datetime.now().strftime("%Y%m%d_%H%M%S")
+    path = "remote_camera/images/" + date + ".png"
+    cv2.imwrite(path, capture_frame)
+    print(path)
