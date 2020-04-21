0f4a6d059e156988bde026cafc155638dc4d65fd
IbukiYoshinaga <yoshinaga.git@gmail.com>
2020-04-22 02:55:12 +0900

    ストリーミング実装

--- backend/remote_camera/__main__.py
+++ backend/remote_camera/__main__.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
-from modules.camera_module import start_camera
-from flask import Flask
+from modules.camera_module import start_camera, generate_video
+from flask import Flask, Response
 import threading
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+app = Flask(__name__)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_video(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == "__main__":
     camera_thread = threading.Thread(target=start_camera)
 
-    app = Flask(__name__)
     server_thread = threading.Thread(
-      target = app.run
+        target=app.run
     )
 
     camera_thread.start()
--- backend/remote_camera/modules/camera_module.py
+++ backend/remote_camera/modules/camera_module.py
@@ -7,19 +7,27 @@ import os
 # ビデオ設定
 video_capture = cv2.VideoCapture(0)
 
+image_count = 0
+
 def start_camera():
-  image_count = 0
+  global image_count
   while True:
     #連番の最後の番号から保存する
     dir = 'remote_camera/images'
-    directory_name_list      = os.listdir(dir)
-    strip_directry_name_list = []
+    directory_name_list       = os.listdir(dir)
+    strip_directry_name_list  = []
     if len(directory_name_list) != 0:
       for directory_name_list in directory_name_list:
         strip_directry_name_list.append(int(os.path.splitext(directory_name_list)[0]))
       image_count = max(strip_directry_name_list) + 1
 
-    ret, capture_frame = video_capture.read()
-    save_image_path = 'remote_camera/images/' + str(image_count) + '.jpg'
+    ret, capture_frame  = video_capture.read()
+    save_image_path     = 'remote_camera/images/' + str(image_count) + '.jpg'
     cv2.imwrite(save_image_path, capture_frame)
     image_count += 1
+
+def generate_video():
+  global image_count
+  while True:
+    yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + open('remote_camera/images/' + str(image_count - 1) + '.jpg' , 'rb').read() + b'\r\n')
