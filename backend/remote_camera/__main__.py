da76c22746a3e15c16dcca5f2d30b6216980a3ff
IbukiYoshinaga <yoshinaga.git@gmail.com>
2020-04-24 01:44:30 +0900

    途中まで

--- backend/remote_camera/__main__.py
+++ backend/remote_camera/__main__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from modules.camera_module import start_camera, generate_video
+from modules.camera_module import start_camera, generate_video, stream_test
 from flask import Flask, Response
 import threading
 import signal
@@ -7,9 +7,14 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 app = Flask(__name__)
 
-@app.route('/video_feed')
-def video_feed():
+@app.route('/stream')
+def camera_stream():
     return Response(generate_video(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/test')
+def test_stream():
+    return Response(stream_test(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     camera_thread = threading.Thread(target=start_camera)
--- backend/remote_camera/modules/camera_module.py
+++ backend/remote_camera/modules/camera_module.py
@@ -3,6 +3,7 @@ import cv2
 from uuid import getnode as get_mac
 from datetime import datetime
 import os
+import time
 
 # ビデオ設定
 video_capture = cv2.VideoCapture(0)
@@ -14,20 +15,32 @@ def start_camera():
   while True:
     #連番の最後の番号から保存する
     dir = 'remote_camera/images'
-    directory_name_list       = os.listdir(dir)
-    strip_directry_name_list  = []
-    if len(directory_name_list) != 0:
-      for directory_name_list in directory_name_list:
-        strip_directry_name_list.append(int(os.path.splitext(directory_name_list)[0]))
-      image_count = max(strip_directry_name_list) + 1
+    in_directory_file_list   = os.listdir(dir)
+    strip_directry_file_list = []
+    if len(strip_directry_file_list) != 0:
+      for directory_file_name in in_directory_file_list:
+        if(type(directory_file_name) is int):
+          strip_directry_file_list.append(
+              int(os.path.splitext(directory_file_name)[0])
+          )
+      image_count = max(strip_directry_file_list) + 1
 
-    ret, capture_frame  = video_capture.read()
-    save_image_path     = 'remote_camera/images/' + str(image_count) + '.jpg'
-    cv2.imwrite(save_image_path, capture_frame)
+    ret, capture_frame = video_capture.read()
+    save_image_path    = 'remote_camera/images/' + str(image_count) + '.jpg'
+    #cv2.imwrite(save_image_path, capture_frame)
     image_count += 1
 
 def generate_video():
   global image_count
   while True:
-    yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + open('remote_camera/images/' + str(image_count - 1) + '.jpg' , 'rb').read() + b'\r\n')
+    if image_count != 0:
+      yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + open('remote_camera/images/' + str(image_count - 1) + '.jpg', 'rb').read() + b'\r\n')
+
+def stream_test():
+  while True:
+    for num in range(700):
+      if num != 0:
+        time.sleep(1)
+        yield (b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + open('remote_camera/images/' + str(num) + '.jpg', 'rb').read() + b'\r\n')
