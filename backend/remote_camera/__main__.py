a2514a1c40a48c9f12edfb2abfbe069d969b66a3
IbukiYoshinaga <yoshinaga.git@gmail.com>
2020-04-28 08:10:59 +0900

    url変更

--- backend/remote_camera/__main__.py
+++ backend/remote_camera/__main__.py
@@ -4,14 +4,13 @@ from modules.camera_module import VideoCameraClass
 
 app = Flask(__name__)
 
-
 def generateFrame(camera):
   while True:
     frame = camera.get_frame()
     yield (b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
+@app.route('/camera_stream')
 def video_feed():
   return Response(generateFrame(VideoCameraClass()),
     mimetype='multipart/x-mixed-replace; boundary=frame')
